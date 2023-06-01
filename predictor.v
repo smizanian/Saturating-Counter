ef7918605766314ba3b1463c66c2023ad26d17e5
smizanian <95073227+smizanian@users.noreply.github.com>
2023-06-01 10:55:08 +0330

    Update predictor.v

--- predictor.v
+++ predictor.v
@@ -6,29 +6,31 @@ module predictor(
     output reg prediction
 );
 
-reg [1:0] count = 2'b11;
+reg [1:0] count=2'b11;
 
-always @(posedge clk) begin
-    if (result) begin
-        if (taken) begin
-            if (count < 2'b11) begin
-                count <= count + 1;
-            end
-        end else begin
-            if (count > 2'b00) begin
-                count = count - 1;
-            end
-    end
-end
 
-always @(posedge clk) begin
-    if (request) begin
-        if (count[1] == 1) begin
-            prediction = 1;
-        end else begin
-            prediction = 0;
-        end
-    end
-end
+
+always @(posedge clk) 
+	begin
+	    if (result) begin
+        	if (taken) begin
+	            if (count < 2'b11) begin
+	                count = count + 1;
+        	    end
+	        end else begin
+        	    if (count > 2'b00) begin
+                	count = count - 1;
+	            end
+		    end
+		end
+		if (request) begin
+        		if (count[1] == 1) begin
+	        	    prediction = 1;
+	        	end else begin
+		            prediction = 0;
+        		end
+		    end
+		end
+
 
 endmodule
