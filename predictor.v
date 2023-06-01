dd96cbfaaf33bf91274e3cfd700809688737a6ab
smizanian <95073227+smizanian@users.noreply.github.com>
2023-06-01 10:34:15 +0330

    Update predictor.v

--- predictor.v
+++ predictor.v
@@ -1,5 +1,39 @@
-module predictor(input wire request, result, clk, taken, output reg prediction);
+module predictor(
+    input wire request,
+    input wire result,
+    input wire clk,
+    input wire taken,
+    output reg prediction
+);
 
-// Your code
+reg [1:0] count;
+
+always @(posedge clk) begin
+    if (result) begin
+        if (taken) begin
+            if (count < 2'b11) begin
+                count <= count + 1;
+            end else begin
+                count <= 2'b11;
+            end
+        end else begin
+            if (count > 2'b00) begin
+                count <= count - 1;
+            end else begin
+                count <= 2'b00;
+            end
+        end
+    end
+end
+
+always @(posedge clk) begin
+    if (request) begin
+        if (count == 2'b11 || count == 2'b10) begin
+            prediction <= 1'b1;
+        end else begin
+            prediction <= 1'b0;
+        end
+    end
+end
 
 endmodule
